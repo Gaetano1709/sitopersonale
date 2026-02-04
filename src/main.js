7df322d409c3721f95921e106472cdd2edd5cce9
Gaetano Rossiello <gagy.rosso.a@gmail.com>
2026-02-04 10:56:15 +0100

    Ho aggiunto l'header

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// Import our custom CSS
+import "/src/style.scss";
+import "bootstrap-icons/font/bootstrap-icons";
+
+// Import all of Bootstrap’s JS
+//import * as bootstrap from "bootstrap";
+
+// Import just what we need
+
+// import 'bootstrap/js/dist/alert';
+import "bootstrap/js/dist/button";
+// import 'bootstrap/js/dist/carousel';
+import "bootstrap/js/dist/collapse";
+// import 'bootstrap/js/dist/dropdown';
+//import 'bootstrap/js/dist/modal';
+// import 'bootstrap/js/dist/offcanvas';
+// import 'bootstrap/js/dist/popover';
+// import 'bootstrap/js/dist/scrollspy';
+// import 'bootstrap/js/dist/tab';
+// import 'bootstrap/js/dist/toast';
+// import 'bootstrap/js/dist/tooltip';
