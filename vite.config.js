70b1649e81469024bd2f4485452531c5f49c558f
Gaetano Rossiello <gagy.rosso.a@gmail.com>
2026-02-05 12:14:14 +0100

    Ho aggiunto in file di configurazione di vite

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+ base: "./",
+ root: resolve(__dirname, "./"),
+ build: { 
+    emptyOutDir: true,
+    outDir: "dist"
+ }
+});
